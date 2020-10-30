cc49f3b6f338d1bd02fc23d3312a7041af503ec1
cxl <1799359457@qq.com>
2020-10-30 15:03:04 +0800

    Initial commit

---
+++ src/main/java/com/cxl/sharing/SharingApplication.java
@@ -0,0 +1,15 @@
+package com.cxl.sharing;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@MapperScan("com.cxl.sharing.mapper")
+public class SharingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SharingApplication.class,args);
+    }
+}
